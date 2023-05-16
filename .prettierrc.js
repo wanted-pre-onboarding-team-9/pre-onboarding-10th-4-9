5cf5d92254d8ef7ad73cdd62178c1f360d181014
Jeon Minji <mjjeon2645@gmail.com>
2023-05-16 15:56:02 +0900

    feat: 타입스크립트, husky, eslint 설정

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 100,
+  semi: true,
+  arrowParens: "always",
+  bracketSameLine: false,
+  trailingComma: "all",
+  useTabs: false,
+};
